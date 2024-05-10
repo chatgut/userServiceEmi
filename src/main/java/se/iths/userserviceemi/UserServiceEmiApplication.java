91270838e54f1d0a64474c484a225c17d890aaa1
Emmelie83 <info@emmeliejohansson.com>
2024-05-10 13:03:51 +0200

    Initial commit

---
+++ src/main/java/se/iths/userserviceemi/UserServiceEmiApplication.java
@@ -0,0 +1,13 @@
+package se.iths.userserviceemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceEmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceEmiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/userserviceemi/UserServiceEmiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.userserviceemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceEmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
