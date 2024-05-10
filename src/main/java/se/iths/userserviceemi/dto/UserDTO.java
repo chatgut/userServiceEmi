20a64f9f53290b8524d701cccd8b060c10c0172a
Emmelie83 <info@emmeliejohansson.com>
2024-05-10 17:39:25 +0200

    Add Docker file

---
+++ src/main/java/se/iths/userserviceemi/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package se.iths.userserviceemi.dto;
+
+public class UserDTO {
+}
---
+++ src/main/java/se/iths/userserviceemi/entity/User.java
@@ -0,0 +1,23 @@
+package se.iths.userserviceemi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+}
